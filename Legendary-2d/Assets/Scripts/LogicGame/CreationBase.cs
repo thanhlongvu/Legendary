9570123531ab3ac73fc0e11551b4579bbcf6cf65
longvt <longlahlug@gmail.com>
2019-08-27 00:28:31 +0700

    add creation base class

---
+++ Legendary-2d/Assets/Scripts/LogicGame/CreationBase.cs
@@ -0,0 +1,16 @@
+﻿
+public abstract class CreationBase : Singleton<CreationBase>
+{
+    public int Health { get; set; }
+    public int Mana { get; set; }
+
+    public virtual void InstanceSkill() { }
+
+    public virtual bool IsDead()
+    {
+        if(Health <= 0)
+            return true;
+
+        return false;
+    }
+}
